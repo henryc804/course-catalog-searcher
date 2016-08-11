e26b186253ff30b8fdaa8c96196acfcadb0ed9e0
henryc804 <henryc804@users.noreply.github.com>
2016-08-11 01:55:17 -0400

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-xhr = new XMLHttpRequest();
-xhr.open("GET", "http://student.mit.edu/catalog/search.cgi?search=6.01&style=verbatim&when=*&termleng=4&days_offered=*&start_time=*&duration=*&total_units=*", true);
-xhr.onreadystatechange = function() {
-	if (xhr.readyState == 4) {
-		var resp = JSON.parse(xhr.responseText);
-		console.log(resp);
-  	}
-};
-xhr.send();
