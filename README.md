8522312c84f391a6a07440193f42c9a96681616a
Henry Cheung <henryc804@gmail.com>
2017-02-25 12:16:47 -0500

    fixed hkn link bug, updated README, added closing up popup upon clicking to hkn/course eval

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # course-catalog-searcher
+A Chrome browser extension for MIT students! If you've ever wondered what class that series of numerals and letters corresponds to, you usually have to copy it down and go to the Course Catalog and search there. Now, you can just highlight the number wherever it is on a webpage, and this will popup a short description of it, with links to course evaluations and more!
+Also, there's a little menu in the top bar where you can also search from there and also jump directly to the Course Catalog instead of searching it.
+
+#### License [MIT](LICENSE)
--- background.js
+++ background.js
@@ -60,7 +60,7 @@ getHTML = function(courseNumber, centerPopupValues) {
 	}
 	courseEvalURL = "https://edu-apps.mit.edu/ose-rpt/subjectEvaluationSearch.htm?termId=&departmentId=&subjectCode=" + courseNumberWithoutJ + "&instructorName=&search=Search";
 	if (courseNumber.slice(0,1) === "6") {
-		hknURL = "https://hkn.mit.edu/new_ug/search/search?utf8=%E2%9C%93&subject_num=" + courseNumber + "&term_season=both&term_year=&button=Search&lec_id=&rec_id=&tut_id=&class_hours_eq=%3E&class_hours=&lab_hours_eq=%3E&lab_hours=&diff_rating_eq=%3E&diff_rating=&prep_hours_eq=%3E&prep_hours=&overall_rating_eq=%3E&overall_rating=&total_hours_eq=%3E&total_hours=";
+		hknURL = "https://underground-guide.mit.edu/search?q=" + courseNumber;
 	} else {
 		hknURL = null;
 	}
@@ -82,10 +82,10 @@ getURL = function() {
 	return url;
 };
 getCourseURL = function() {
-	// windowOfPopup.close();
+	windowOfPopup.close();
 	return courseEvalURL;
 };
 gethknURL = function() {
-	// windowOfPopup.close();
+	windowOfPopup.close();
 	return hknURL;
 };
